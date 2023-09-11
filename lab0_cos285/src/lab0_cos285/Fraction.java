cf8288b04c796fa913f03e03e5da903027a6f963
Colby Cormier <nerdygamerz207@gmail.com>
2023-09-11 14:27:47 -0400

    Fraction implementation

--- lab0_cos285/src/lab0_cos285/Fraction.java
+++ lab0_cos285/src/lab0_cos285/Fraction.java
@@ -2,6 +2,19 @@ package lab0_cos285;
 
 public class Fraction implements INumber{
 
+	// Fields
+	double fractionValue;
 	
+	// Constructor
+	public Fraction(double value)
+	{
+		fractionValue = value;
+	}
+	
+	// Methods
+	public double plus(double input)
+	{
+		return (fractionValue + input);
+	}
 	
 }
