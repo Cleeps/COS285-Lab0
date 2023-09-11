3c73d9525ff50ff3bd228df6d2c58756c5943518
Nikan <nikankad@protonmail.com>
2023-09-11 14:26:45 -0400

    init methods

--- lab0_cos285/src/lab0_cos285/Fraction.java
+++ lab0_cos285/src/lab0_cos285/Fraction.java
@@ -2,6 +2,35 @@ package lab0_cos285;
 
 public class Fraction implements INumber{
 
-	
+	@Override
+	public Object plus(Object input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object minus(Object input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object divide(Object input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object multiply(Object input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
 }
